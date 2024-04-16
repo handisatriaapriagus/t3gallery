6b95c52eb2429f0913d5d3170853a253b5ac1597
handisatria <handi@admin.spacearcade.online>
2024-04-16 10:37:13 +0700

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 const mockUrls = [
-  "https://utfs.io/f/aab1de05-ac53-40b3-8806-1a05d6e1f1ca-1vu9e.png",
-  "https://utfs.io/f/affd17e5-5f43-4bd4-b8b4-1b6e6f911d5e-1ppxj8.png",
-  "https://utfs.io/f/c4f4c334-f8be-408e-8537-c4e8fd338d73-1tx3i.png",
-  "https://utfs.io/f/d96396f6-ec54-4d6b-8c7e-8fbfb4ecf45d-ecsrbs.png",
+  "https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
+  "https://utfs.io/f/93fe9634-fea2-4314-956b-7ae7f700ae5b-qa0vsq.png",
+  "https://utfs.io/f/4709ed08-e620-4c42-9d65-94af53585014-lf3twj.png",
+  "https://utfs.io/f/c494ae05-dd1a-459a-b09d-bbe37a140853-qikpqu.1.png",
 ];
 
 const mockImages = mockUrls.map((url,index) => ({
@@ -16,14 +16,13 @@ const mockImages = mockUrls.map((url,index) => ({
 export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap">
-        {mockImages.map((image) => (
-          <div key={image.id} className="mw-1/2 p-4">
-            <img src={image.url}  />
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
           </div>
         ))}
       </div>
-      Hello (gallery in progress)
     </main>
   );
 }
